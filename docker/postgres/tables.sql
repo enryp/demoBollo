456608a4ec493bd1d8685fe50d71d43ac46b9c99
enricop <enrico.peroglio@alliumtech.it>
2022-12-28 02:10:33 +0100

    DDL

--- docker/postgres/tables.sql
+++ docker/postgres/tables.sql
@@ -1,5 +1,13 @@
+CREATE SEQUENCE public.veicoli_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
 CREATE TABLE public.veicolo (
-	id int4 NOT NULL,
+	id int4 NOT NULL DEFAULT nextval('veicoli_seq'::regclass),
 	targa varchar NULL,
 	tipo varchar NULL,
 	codice_fiscale varchar NULL,
@@ -7,3 +15,4 @@ CREATE TABLE public.veicolo (
 	immatricolazione timestamp NULL,
 	CONSTRAINT veicolo_pk PRIMARY KEY (id)
 );
+
