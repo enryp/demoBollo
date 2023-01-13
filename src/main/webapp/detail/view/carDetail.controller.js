1c3d55982e301f826978d5f29eaadadfe1b4c0af
enricop <enrico.peroglio@alliumtech.it>
2023-01-13 01:20:42 +0100

    aggiunta notifica bollo in scadenza

--- src/main/webapp/detail/view/carDetail.controller.js
+++ src/main/webapp/detail/view/carDetail.controller.js
@@ -30,7 +30,6 @@
         //{"targa":"EN300ZJ","scadenza":"SETTEMBRE 2023","ultimoGiornoUtile":"31/10/2022","validita":12,"totale":267.37,"tassa":262.08,"sanzione":4.38,"interesse":0.91}
         self.bollo = {};
         self.editEnabled = false;    
-        
 
         // State parameters
         //self.currentState = $rootScope.toState.name;
@@ -38,6 +37,9 @@
 
         self.backToList = backToList;
         self.enableEdit = enableEdit;
+
+        self.bolloInScadenza = false;
+        self.bolloScaduto = false;
         
 
 
@@ -65,6 +67,33 @@
             });
         }
 
+        function checkScadenzaLimit(){
+            let now = moment(); 
+            console.log("@@now "+now.format('YYYY-MM-DD'));
+            let ultimoGiornoUtile = moment(self.bollo.ultimoGiornoUtile, "DD/MM/YYYY"); 
+            console.log("@@ultimoGiornoUtile "+ultimoGiornoUtile.format('YYYY-MM-DD')); 
+            let sogliaScadenzaBollo = ultimoGiornoUtile.subtract(3, 'months');
+            console.log("@@sogliaScadenzaBollo "+sogliaScadenzaBollo.format('YYYY-MM-DD'));
+
+            if(now.isAfter(ultimoGiornoUtile)){
+                self.bolloScaduto = true;
+                self.bolloInScadenza = false;
+            }
+            else if(now.isBetween(sogliaScadenzaBollo,ultimoGiornoUtile)){
+                self.bolloScaduto = false;
+                self.bolloInScadenza = true;
+            }
+            else{
+                self.bolloInScadenza = false;
+                self.bolloScaduto = false;
+            }
+            
+
+        }
+
+        angular.element(function () {
+            checkScadenzaLimit();
+        });
 
     }
 })();
